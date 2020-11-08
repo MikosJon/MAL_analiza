571471deeaf92e117b33c5ffe0a2504bda647b75
Jon Mikoš <atletjm@gmail.com>
2020-11-08 23:26:18 +0100

    očiščeni podatki

--- podatki.py
+++ podatki.py
@@ -4,7 +4,7 @@ import re
 import requests
 import time
 
-def cleanup(title, data):
+def cleanup(title, data, popularity):
     '''
     data = (
         type,
@@ -37,14 +37,23 @@ def cleanup(title, data):
         'rating'    : Str
         'score'     : Float
         'votes'     : Int
+        'popularity': Int
     ]
     '''
     title = title
-    series_type = data[0]
+
+    if data[0] and 'div' in data[0]:
+        series_type = 'Music'
+    else:
+        series_type = data[0]
+
     episodes = int(data[1]) if data[1] else None
     status = data[2]
 
-    premiered = data[3]
+    if data[3] and 'div' in data[3]:
+        premiered = ''
+    else:
+        premiered = data[3]
 
     producers = cleanup_title_re.findall(data[4]) or []
     licensors = cleanup_title_re.findall(data[5]) or []
@@ -54,16 +63,20 @@ def cleanup(title, data):
 
     genres = cleanup_title_re.findall(data[8]) or []
 
-    h1, m1, m2 = cleanup_duration_re.findall(data[9])[0]
-    if m2:
-        duration = int(m2)
+    duration_match = cleanup_duration_re.findall(data[9])
+    if duration_match:
+        h1, m1, m2 = duration_match[0]
+        if m2:
+            duration = int(m2)
+        else:
+            total = 0
+            if h1:
+                total += 60 * int(h1)
+            if m1:
+                total += int(m1)
+            duration = total if total else None
     else:
-        total = 0
-        if h1:
-            total += 60 * int(h1)
-        if m1:
-            total += int(m1)
-        duration = total if total else None
+        duration = None
 
 
     rating = data[10].replace('amp;', '') if data[10] else None
@@ -84,7 +97,8 @@ def cleanup(title, data):
         'duration'   : duration,
         'rating'     : rating,
         'score'      : score,
-        'votes'      : votes
+        'votes'      : votes,
+        'popularity' : popularity
     }
     return clean_data
 
@@ -125,15 +139,60 @@ re_data = [
     ]
 block_match_regexes = [re.compile(pattern, flags=re.DOTALL) for pattern in re_data]
 
-#-----------------------------------------------------------------------------------------
-# Lokacije in shranjevanje glavne strani
+def write(rows, *, main_filename, producer_filename, licensor_filename, studio_filename, genre_filename):
+    main_headers = [
+        'title',
+        'series_type',
+        'episodes',
+        'status',
+        'premiered',
+        'source',
+        'duration',
+        'rating',
+        'score',
+        'votes',
+        'popularity'
+    ]
+
+    main_rows = []
+    producer_rows = []
+    licensor_rows = []
+    studio_rows = []
+    genre_rows = []
+
+    for row in rows:
+        new_row = {}
+
+        for key, value in row.items():
+            if key in main_headers:
+                new_row[key] = value
+
+            elif key == 'producers':
+                for producer in value:
+                    producer_rows.append({'title': row['title'], 'producer': producer})
+
+            elif key == 'licensors':
+                for licensor in value:
+                    licensor_rows.append({'title': row['title'], 'licensor': licensor})
+
+            elif key == 'studios':
+                for studio in value:
+                    studio_rows.append({'title': row['title'], 'studio': studio})
+
+            elif key == 'genres':
+                for genre in value:
+                    genre_rows.append({'title': row['title'], 'genre': genre})
+
+        main_rows.append(new_row)
+
+    orodja.zapisi_csv(main_rows, main_headers, os.path.join(data_dir, main_filename))
+    orodja.zapisi_csv(producer_rows, ['title', 'producer'], os.path.join(data_dir, producer_filename))
+    orodja.zapisi_csv(licensor_rows, ['title', 'licensor'], os.path.join(data_dir, licensor_filename))
+    orodja.zapisi_csv(studio_rows, ['title', 'studio'], os.path.join(data_dir, studio_filename))
+    orodja.zapisi_csv(genre_rows, ['title', 'genre'], os.path.join(data_dir, genre_filename))
+
 
 def main():
-    homepage_url = 'https://myanimelist.net/topanime.php?type=bypopularity'
-
-    data_dir = 'data'
-    homepage_filename = os.path.join(data_dir, 'homepage.html')
-
     download = True
     if os.path.isdir(data_dir):
         msg = 'Direktorij s podatki že obstaja. Ali želite ponovno naložiti vse podatke? [Y/N]: '
@@ -193,81 +252,36 @@ def main():
             block = comp_re_block.findall(page)[0]
             data = block_match(block)
 
-            rows.append(cleanup(title, data))
+            rows.append(cleanup(title, data, idx+1))
         except Exception:
-            fails.append(url)
+            fails.append((title, url, idx+1))
             continue
 
     orodja.pripravi_imenik(data_dir)
     with open(os.path.join(data_dir, 'fails'), 'w') as f:
-        for url in fails:
-            f.write(f'{url}\n')
-#-----------------------------------------------------------------------------------------
-# Sortiranje prečiščenih podatkov v vrste za csv datoteke
+        for title, url, pop in fails:
+            f.write(f'{title},{url},{pop}\n')
 
-    main_headers = [
-        'title',
-        'series_type',
-        'episodes',
-        'status',
-        'premiered',
-        'source',
-        'duration',
-        'rating',
-        'score',
-        'votes'
-    ]
-
-    main_rows = []
-    producer_rows = []
-    licensor_rows = []
-    studio_rows = []
-    genre_rows = []
-
-    for row in rows:
-        new_row = {}
-
-        for key, value in row.items():
-            if key in main_headers:
-                new_row[key] = value
-
-            elif key == 'producers':
-                for producer in value:
-                    producer_rows.append({'title': row['title'], 'producer': producer})
-
-            elif key == 'licensors':
-                for licensor in value:
-                    licensor_rows.append({'title': row['title'], 'licensor': licensor})
-
-            elif key == 'studios':
-                for studio in value:
-                    studio_rows.append({'title': row['title'], 'studio': studio})
-
-            elif key == 'genres':
-                for genre in value:
-                    genre_rows.append({'title': row['title'], 'genre': genre})
-
-        main_rows.append(new_row)
+    write(
+        rows,
+        main_filename=main_csv_name,
+        producer_filename=producer_csv_name,
+        licensor_filename=licensor_csv_name,
+        studio_filename=studio_csv_name,
+        genre_filename=genre_csv_name
+    )
 
 #-----------------------------------------------------------------------------------------
-# pisanje podatkov v csv datoteke
 
-    main_csv_name = 'serije.csv'
-    orodja.zapisi_csv(main_rows, main_headers, os.path.join(data_dir, main_csv_name))
+genre_csv_name = 'žanri.csv'
+studio_csv_name = 'studiji.csv'
+licensor_csv_name = 'licensorji.csv'
+producer_csv_name = 'producerji.csv'
+main_csv_name = 'serije.csv'
 
-    producer_csv_name = 'producerji.csv'
-    orodja.zapisi_csv(producer_rows, ['title', 'producer'], os.path.join(data_dir, producer_csv_name))
-
-    licensor_csv_name = 'licensorji.csv'
-    orodja.zapisi_csv(licensor_rows, ['title', 'licensor'], os.path.join(data_dir, licensor_csv_name))
-
-    studio_csv_name = 'studiji.csv'
-    orodja.zapisi_csv(studio_rows, ['title', 'studio'], os.path.join(data_dir, studio_csv_name))
-
-    genre_csv_name = 'žanri.csv'
-    orodja.zapisi_csv(genre_rows, ['title', 'genre'], os.path.join(data_dir, genre_csv_name))
-
-
+data_dir = 'data'
+homepage_url = 'https://myanimelist.net/topanime.php?type=bypopularity'
+homepage_filename = os.path.join(data_dir, 'homepage.html')
 
 if __name__ == '__main__':
     main()
